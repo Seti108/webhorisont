989a5f36d7968a5b54983187c889d47a7c2d192a
Seti108 <seti108@gmail.com>
2021-07-30 13:15:11 +0200

    mobile paket

--- script.js
+++ script.js
@@ -26,3 +26,36 @@ closeMobileMenu.addEventListener('click', function () {
   mobileMenu.style.display = 'none';
   menuToggle = false;
 });
+
+// Priser & Paket
+
+const termsBtn = document.querySelectorAll('#bill-terms');
+const termsPopup = document.querySelectorAll('.terms');
+const bundlesSection = document.querySelector('.bundles');
+// Open/close terms
+let termsToggle = false;
+termsBtn.forEach((btn, i) => {
+  btn.addEventListener('click', function () {
+    if (!termsToggle) {
+      termsPopup[i].style.display = 'block';
+      termsToggle = true;
+      return;
+    }
+    if (termsToggle) {
+      termsPopup.forEach((popup) => {
+        popup.style.display = 'none';
+        termsToggle = false;
+      });
+    }
+  });
+});
+
+// bundlesSection.addEventListener('click', function (e) {
+//   if (termsToggle == true) {
+//     console.log('Clicking');
+//     termsPopup.forEach((popup) => {
+//       popup.style.display = 'none';
+//       termsToggle = false;
+//     });
+//   }
+// });
